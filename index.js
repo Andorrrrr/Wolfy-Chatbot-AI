a458520f92515134992689b44b57dc5b8664e04a
Andorrrrr <andorrturtle@gmail.com>
2022-04-10 20:51:02 +0200

    test

--- index.js
+++ index.js
@@ -44,22 +44,5 @@ const client = new Discord.Client({
 client.prefix = 'w!';
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.json'));
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.push(command.data.toJSON());
-}
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
-	}
-}
-
 mongoose.init();
 client.login(process.env.TOKEN);
